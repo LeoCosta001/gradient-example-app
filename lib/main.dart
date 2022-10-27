64ef0387c125486dcd74b21104c30129d1aecb59
LeoCosta <leocosta.black@hotmail.com>
2022-10-27 08:56:41 -0300

    Create border and text gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/gradient_borders.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 void main() {
+  const List<Color> gradientColors = [Color(0xFFFDEF80), Color(0xFF7CEF94)];
+
   runApp(
     MaterialApp(
-      home: Container(),
+      home: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          border: const GradientBoxBorder(
+            gradient: LinearGradient(
+              colors: gradientColors,
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(28),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GradientText(
+                'Eat\nSleep\nCode\nRepeat',
+                style: const TextStyle(
+                  fontSize: 60.0,
+                  decoration: TextDecoration.none,
+                ),
+                gradientDirection: GradientDirection.ttb,
+                colors: gradientColors,
+              ),
+            ],
+          ),
+        ),
+      ),
     ),
   );
 }
