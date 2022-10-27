30277bad619fe1d919b2ca41565035b84f338ebc
LeoCosta <leocosta.black@hotmail.com>
2022-10-27 09:19:01 -0300

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Gradient example',
       theme: ThemeData(),
       home: const HomePage(),
